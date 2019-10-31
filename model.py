5ea2481f9b09f69d16f85b4e92659c5aed2e65bf
Symeonw <Symeonwhite16@gmail.com>
2019-10-31 16:00:16 -0500

    update

--- model.py
+++ model.py
@@ -6,12 +6,13 @@ df["tenure_years"] = [n/12 for n in df.tenure]
 df_2 = df.drop(columns = ["customer_id", "payment_type_id", "internet_service_type_id", "contract_type_id"], inplace=True)
 
 
-
-train, test = train_test_split(df_2, train_size = .75, random_state = 123)
-X_train = train.drop(columns = "churn")
-X_test = test.drop(columns = "churn")
-y_train = train.churn
-y_test = test.churn
+df_2 = get_telco()
+df_2.drop(columns = ["customer_id", "payment_type_id", "internet_service_type_id", "contract_type_id"], inplace=True)
+train_2, test_2 = train_test_split(df_2, train_size = .75, random_state = 123)
+X_train_2 = train.drop(columns = "churn")
+X_test_2 = test.drop(columns = "churn")
+y_train_2 = train.churn
+y_test_2 = test.churn
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
@@ -21,8 +22,8 @@ from sklearn.compose import ColumnTransformer
 
 numeric_transformer = Pipeline(steps=[('scaler', StandardScaler())])
 categorical_transformer = Pipeline(steps=[('onehot', OneHotEncoder(handle_unknown='ignore'))])
-numeric_features = train.select_dtypes(include=['int64', 'float64']).columns
-categorical_features = train.select_dtypes(include=['object']).drop(['churn'], axis=1).columns
+numeric_features = train._2select_dtypes(include=['int64', 'float64']).columns
+categorical_features = train_2.select_dtypes(include=['object']).drop(['churn'], axis=1).columns
 preprocessor = ColumnTransformer(transformers=[('num', numeric_transformer, numeric_features),('cat', categorical_transformer, categorical_features)])
 from sklearn.metrics import accuracy_score, log_loss
 from sklearn.neighbors import KNeighborsClassifier
@@ -43,6 +44,6 @@ classifiers = [
 for classifier in classifiers:
     pipe = Pipeline(steps=[('preprocessor', preprocessor),
                       ('classifier', classifier)])
-    pipe.fit(X_train, y_train)   
+    pipe.fit(X_train_2, y_train_2)   
     print(classifier)
-    print("model score: %.3f" % pipe.score(X_test, y_test))
+    print("model score: %.3f" % pipe.score(X_test_2, y_test_2))
